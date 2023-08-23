d0b5244b29a0c16665109e88dcd2c34da64b76dd
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-08-23 17:37:11 -0300

    We dont offer anymore

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -12,7 +12,7 @@ export default function MainPage(){
         <Home />
         <WhoWeAre />
         <WhatIsAqua />
-        <WeOffer />
+       {/**  <WeOffer />*/}
         </StyledMainPage>
     )
 }
